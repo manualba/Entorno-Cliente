ba712eda50f0c47c3c2e2b3ed548282983be3f2e
manualba <45296827+manualba@users.noreply.github.com>
2019-12-03 17:20:19 +0100

    metodoPago-JQuery

--- --Proyecto1erTrimestre--/Sprint5/metodoPago/pago.js
+++ --Proyecto1erTrimestre--/Sprint5/metodoPago/pago.js
@@ -1,145 +1,251 @@
-//NombreUsuario, gmail, contraseña
-//NumTarjeta, Cvv, FechaVegecimiento, Nombre de usuario de tarjeta
+let listaUsuarios = [];
 
+//Usuario
+$(() => $("#nombre").keyup(() => { 
+    let esCorrecto = true;
+    let inputNombre = document.getElementById("nombre");
+    let valor = inputNombre.value.trim();
+    let listaErrores = document.getElementById("erroresNombre");
+    listaErrores.innerHTML = "";
+    inputNombre.classList.remove("inputErroneo");
+    inputNombre.classList.remove("inputCorrecto");
 
+    if(!/^[a-zA-Z0-9]+$/.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Solo se permiten caracteres normales y sin espacios";
+        listaErrores.appendChild(divError);
+    }
 
-$(() => $("#nombre").keyup(() => { //() => es igual a "function(){...}"
-    let resultado = false;
-    let inputNombre = $("#nombre");
-    inputNombre.removeClass("error");
-    inputNombre.removeClass("correcto");
-    let valorNombre = inputNombre.val().trim();
-    if(valorNombre !== ""
-        && valorNombre.match(/^[A-Z]{3,}[0-9]{1,}$/)
-    ){
-        resultado = true;
-        inputNombre.addClass("correcto");
-        if(inputNombre.next().length > 0 ){
-            inputNombre.next().remove();
-        }
+    if(valor.length < 4){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu usuario debe de tener al menos 4 caracteres";
+        listaErrores.appendChild(divError);
+    }
+
+    if(esCorrecto){
+        inputNombre.classList.add("inputCorrecto");
     }else{
-        inputNombre.addClass("error");
-        if(inputNombre.next().length === 0 ){
-            inputNombre.after("<span>ERROR EN EL NOMBRE</span>");
-        }
+        inputNombre.classList.add("inputErroneo");
     }
 
-
-    return resultado;
+    return esCorrecto;
 }));
 
-$(() => $("#tiempo").keyup(() => { 
-    let resultado = false;
-    let inputTiempo = $("#tiempo");
-    inputTiempo.removeClass("error");
-    inputTiempo.removeClass("correcto");
-    let valorTiempo = inputTiempo.val().trim();
+//Password
+$(() => $("#pass").keyup(() => { 
+    let esCorrecto = true;
+    let inputPass = document.getElementById("pass");
+    let valor = inputPass.value.trim();
+    let listaErrores = document.getElementById("erroresPass");
+    listaErrores.innerHTML = "";
+    inputPass.classList.remove("inputErroneo");
+    inputPass.classList.remove("inputCorrecto");
 
-    if(valorTiempo !== ""
-        && valorTiempo.match(/^[0-9]{2,}$/)
-        
-    ){
-        resultado = true;
-        inputTiempo.addClass("correcto");
-        if(inputTiempo.next().length > 0 ){
-            inputTiempo.next().remove();
-        }
-        
+    if (!/(.*[a-zA-Z0-9]+.*){6,}/.test(valor)) { // 6 caracteres 
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu password debe de tener minimo 6 caracteres";
+        listaErrores.appendChild(divError);
+    }
+
+    if (!/(.*[A-Z]+.*){1,}/.test(valor)){ // 1 mayuscula
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu password debe tener una mayuscula";
+        listaErrores.appendChild(divError);
+    }
+
+    if (!/(.*[\!\?\-\$\_]+.*){1,}/.test(valor)){ // 1 caracter especial
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu password debe tener un caracter especial";
+        listaErrores.appendChild(divError);
+    }
+
+    if (esCorrecto){
+        inputPass.classList.add("inputCorrecto");
     }else{
-        inputTiempo.addClass("error");
-        if(inputTiempo.next().length === 0 ){
-            inputTiempo.after("<span>ERROR EN EL TIEMPO</span>");
-        }
+        inputPass.classList.add("inputErroneo");
     }
 
-
-    return resultado;
+    return esCorrecto;
 
 }));
 
-$(() => $("#tipo").change(() => { 
-    let resultado = false;
-    let inputTipo = $("#tipo");
-    inputTipo.removeClass("error");
-    inputTipo.removeClass("correcto");
-    
-        if ($('#tipo').val() !== ''){
-        resultado = true;
-        inputTipo.addClass("correcto");
-        if(inputTipo.next().length > 0 ){
-            inputTipo.next().remove();
-        }
-       
-        }else{
-        inputTipo.addClass("error");
-        if(inputTipo.next().length === 0 ){
-            inputTipo.after("<span>ERROR EN EL TIPO</span>");
-        }
+//Email
+$(() => $("#email").keyup(() => { 
+    let esCorrecto = true;
+    let inputEmail = document.getElementById("email");
+    let valor = inputEmail.value.trim();
+    let listaErrores = document.getElementById("erroresEmail");
+    listaErrores.innerHTML = "";
+    inputEmail.classList.remove("inputErroneo");
+    inputEmail.classList.remove("inputCorrecto");
+
+
+    if (!valor.match(/^[a-zA-Z0-9]+/g)) {
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu Gmail debe empezar por una letra o número";
+        listaErrores.appendChild(divError);
     }
 
+    if (!(valor.endsWith(".com") || valor.endsWith(".es"))){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu gmail debe terminar con .com o .es";
+        listaErrores.appendChild(divError);
+    }
 
-    return resultado;
+    if (!valor.includes("@")){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu gmail debe tener un \"@\"";
+        listaErrores.appendChild(divError);
+    }
+
+    if (esCorrecto){
+        inputEmail.classList.add("inputCorrecto");
+    }else{
+        inputEmail.classList.add("inputErroneo");
+    }
+
+    return esCorrecto;
 
 }));
 
-$(() => $("#beneficios").click(() => { //() => es igual a "function(){...}"
-    let resultado = false;
-    let inputBeneficios = $("#beneficios");
-    inputBeneficios.removeClass("error");
-    inputBeneficios.removeClass("correcto");
+//NombrePropietario
+$(() => $("#nomProp").keyup(() => { 
+    let esCorrecto = true;
+    let inputNomProp = document.getElementById("nomProp");
+    let valor = inputNomProp.value.trim();
+    let listaErrores = document.getElementById("erroresNomProp");
+    listaErrores.innerHTML = "";
+    inputNomProp.classList.remove("inputErroneo");
+    inputNomProp.classList.remove("inputCorrecto");
 
-        if ($('input[name="b"]').is(':checked')){
-        resultado = true;
-        inputBeneficios.addClass("correcto");
+    if(!/^[a-z A-Z]+$/.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Solo se permiten letras";
+        listaErrores.appendChild(divError);
+    }
+
+    if(valor.length < 5){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Tu nombre debe de tener al menos 5 letras";
+        listaErrores.appendChild(divError);
+    }
+
+    if(esCorrecto){
+        inputNomProp.classList.add("inputCorrecto");
+    }else{
+        inputNomProp.classList.add("inputErroneo");
+    }
+
+    return esCorrecto;
+}));
+
+
+//NombrePropietario
+$(() => $("#numT").keyup(() => { 
+    let esCorrecto = true;
+    let inputNumT = document.getElementById("numT");
+    let valor = inputNumT.value.trim();
+    let listaErrores = document.getElementById("erroresNumT");
+    listaErrores.innerHTML = "";
+    inputNumT.classList.remove("inputErroneo");
+    inputNumT.classList.remove("inputCorrecto");
+
+    if(!/^[0-9]+$/.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Solo se permiten numeros";
+        listaErrores.appendChild(divError);
+    }
+
+    if(!/^.{16}$/.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "El numero de tu tarjeta es erroneo, debe tener 16 numeros";
+        listaErrores.appendChild(divError);
+    }
+
+    if(esCorrecto){
+        inputNumT.classList.add("inputCorrecto");
+    }else{
+        inputNumT.classList.add("inputErroneo");
+    }
+
+    return esCorrecto;
+}));
+
+
+//CVV
+$(() => $("#cvv").keyup(() => { 
+    let esCorrecto = true;
+    let inputCvv = document.getElementById("cvv");
+    let valor = inputCvv.value.trim();
+    let listaErrores = document.getElementById("erroresCvv");
+    listaErrores.innerHTML = "";
+    inputCvv.classList.remove("inputErroneo");
+    inputCvv.classList.remove("inputCorrecto");
+
+    if(!/^[0-9]+$/.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "Solo se permiten numeros";
+        listaErrores.appendChild(divError);
+    }
+
+    if(!/^.{3}$/.test(valor)){
+        esCorrecto = false;
+        let divError = document.createElement("div");
+        divError.innerHTML = "El CVV debe contener 3 numeros";
+        listaErrores.appendChild(divError);
+    }
+
+    if(esCorrecto){
+        inputCvv.classList.add("inputCorrecto");
+    }else{
+        inputCvv.classList.add("inputErroneo");
+    }
+
+    return esCorrecto;
+}));
+
+
+//Formulario
+$(() => $("#formulario").click(() => { 
+    levent.preventDefault();
+
+    let esFormularioCorrecto = false;
+    let esCorrectoNombre = $("#nombre");
+    let esCorrectoEmail = $("#email");
+    let esCorrectoPass = $("#pass");
+    let esCorrectoNomProp = $("#nomProp");
+    let esCorrectoNumT = $("#numT");
+    let esCorrectoCvv = $("#cvv");
+    //let esCorrectoFechaV = $("#fechaV");
     
-        }else{
-            inputBeneficios.addClass("error");
-        if(inputBeneficios.next().length === 0 ){
-            inputBeneficios.after("<span>ERROR EN LOS BENEFICIOS</span>");
-        }
-}
 
-
-return resultado;
-}));
-
-
-$(() => $("#personal").click(() => { //() => es igual a "function(){...}"
-    let resultado = false;
-    let inputPersonal = $("#personal");
-    inputPersonal.removeClass("error");
-    inputPersonal.removeClass("correcto");
-
-    if ($('input[name="p"]').is(':checked')){
-        resultado = true;
-        inputPersonal.addClass("correcto");
-        
     
-        }else{
-            inputPersonal.addClass("error");
-        if(inputPersonal.next().length === 0 ){
-            inputPersonal.after("<span>ERROR EN EL TIPO</span>");
-        }
-}
+    if (esCorrectoNombre &&  esCorrectoEmail && esCorrectoPass 
+        && esCorrectoNomProp && esCorrectoNumT && esCorrectoCvv 
+        && $("#fechaV").text().includes("")
+        ) {
+        esFormularioCorrecto = true;
 
+        alert("Reservado con exito!");
+        console.log(listaUsuarios);
+  
+    } else {
+        alert("ERROR! Complete bien sus datos");
+    }
+    
 
-return resultado;
 }));
-
-
-function validarFormulario(){
-    $("#submit").attr("disabled",true);
-    let resultado = false;
-
-    resultado = esNombreCorrecto()
-                && esTiempoCorrecto()
-                && esTipoCorrecto()
-                && esBeneficiosCorrecto()
-                && esPersonalCorrecto();
-
-
-    $("#submit").removeAttr("disabled");
-    return resultado;
-}
-
 
