d6455d3f8be1e6555bfdf11c0e22daf733b0c436
manualba <45296827+manualba@users.noreply.github.com>
2020-01-16 17:48:35 +0100

    servidor

---
+++ 2TRIMESTRE/Tema5/servidor/datosBuscar.php
@@ -0,0 +1,30 @@
+ <?php
+   
+   sleep(1);
+   
+   header('Content-Type: application/json');
+   header('Cache-Control: no-cache, must-revalidate');
+   
+       $servidor = "localhost";
+       $usuario = "root";
+       $contrasena = "";  
+       $basededatos = "hoteles";
+   
+       $conexion = mysqli_connect($servidor, $usuario, $contrasena, $basededatos);
+       
+       $consulta = "SELECT * FROM clientes WHERE nombre LIKE '%" . $_POST['nombre'] . "%' ";
+   
+       $resultadoBusqueda = mysqli_query($conexion, $consulta);
+   
+       $resultadoB = array();
+   
+       while ($columna = mysqli_fetch_array( $resultadoBusqueda )){
+           $resultadoB[] = $columna;
+       }
+       echo json_encode($resultadoB);
+   
+       mysqli_close($conexion);
+   
+   
+   ?>
+   
